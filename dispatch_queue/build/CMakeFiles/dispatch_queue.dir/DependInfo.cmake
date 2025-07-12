7fa5b8aa7575fba791cc05c1c0915543668cc527
zhouli.gavin <zhouli.gavin@bigo.sg>
2025-07-12 16:55:30 +0800

    chore:更换目录

---
+++ dispatch_queue/build/CMakeFiles/dispatch_queue.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/3rdparty/ThreadRWLock.cpp" "CMakeFiles/dispatch_queue.dir/3rdparty/ThreadRWLock.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/3rdparty/ThreadRWLock.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/TaskGroup.cpp" "CMakeFiles/dispatch_queue.dir/TaskGroup.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/TaskGroup.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/TaskQueue.cpp" "CMakeFiles/dispatch_queue.dir/TaskQueue.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/TaskQueue.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/TaskQueueFactory.cpp" "CMakeFiles/dispatch_queue.dir/TaskQueueFactory.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/TaskQueueFactory.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/SerialThreadPool.cpp" "CMakeFiles/dispatch_queue.dir/backend/SerialThreadPool.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/SerialThreadPool.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/GroupImpl.cpp" "CMakeFiles/dispatch_queue.dir/backend/GroupImpl.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/GroupImpl.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/IThreadPool.cpp" "CMakeFiles/dispatch_queue.dir/backend/IThreadPool.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/IThreadPool.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/LWBarrier.cpp" "CMakeFiles/dispatch_queue.dir/backend/LWBarrier.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/LWBarrier.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/ParallelQueueImpl.cpp" "CMakeFiles/dispatch_queue.dir/backend/ParallelQueueImpl.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/ParallelQueueImpl.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/ParallelThreadPool.cpp" "CMakeFiles/dispatch_queue.dir/backend/ParallelThreadPool.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/ParallelThreadPool.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/Semaphore.cpp" "CMakeFiles/dispatch_queue.dir/backend/Semaphore.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/Semaphore.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/SerialQueueImpl.cpp" "CMakeFiles/dispatch_queue.dir/backend/SerialQueueImpl.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/SerialQueueImpl.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/SysUtils.cpp" "CMakeFiles/dispatch_queue.dir/backend/SysUtils.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/SysUtils.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/TaskQueueFactoryImpl.cpp" "CMakeFiles/dispatch_queue.dir/backend/TaskQueueFactoryImpl.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/TaskQueueFactoryImpl.cpp.o.d"
+  "/Users/bigo/Documents/Doc/test-projects/test/dispatch_queue/backend/WorkThread.cpp" "CMakeFiles/dispatch_queue.dir/backend/WorkThread.cpp.o" "gcc" "CMakeFiles/dispatch_queue.dir/backend/WorkThread.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ dispatch_queue/build/CMakeFiles/dispatch_queue.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dispatch_queue.dir/3rdparty/ThreadRWLock.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/3rdparty/ThreadRWLock.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/TaskGroup.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/TaskGroup.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/TaskQueue.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/TaskQueue.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/TaskQueueFactory.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/TaskQueueFactory.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/SerialThreadPool.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/SerialThreadPool.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/GroupImpl.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/GroupImpl.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/IThreadPool.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/IThreadPool.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/LWBarrier.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/LWBarrier.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/ParallelQueueImpl.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/ParallelQueueImpl.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/ParallelThreadPool.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/ParallelThreadPool.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/Semaphore.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/Semaphore.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/SerialQueueImpl.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/SerialQueueImpl.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/SysUtils.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/SysUtils.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/TaskQueueFactoryImpl.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/TaskQueueFactoryImpl.cpp.o.d"
+  "CMakeFiles/dispatch_queue.dir/backend/WorkThread.cpp.o"
+  "CMakeFiles/dispatch_queue.dir/backend/WorkThread.cpp.o.d"
+  "lib/libdispatch_queue.dylib"
+  "lib/libdispatch_queue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dispatch_queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
